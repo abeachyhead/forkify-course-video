090ace572d1c30f5f03a076a16c9858a5a869787
abeachyhead <amybeachyhead@gmail.com>
2021-12-05 09:21:45 +0200

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -127,6 +127,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('welcome to the application!');
+};
+
 // Event Listeners (Subscriber)
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -136,5 +140,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
